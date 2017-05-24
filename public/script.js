c1daa80f597f7f973210ed4429c890a438bc50e7
aaronev <aarone.villanueva@gmail.com>
2017-05-23 17:50:44 -0700

    finished all functions need to finish edit function

--- public/script.js
+++ public/script.js
@@ -1,8 +1,122 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+let defaultForm = {
+  'title': 'Title',
+  'image': 'Image',
+  'author': 'Author',
+  'releaseDate': 'Release Date'
+}
 
-  // code in here
-  // event.preventDefault()
-  
-});
+function addBook(book){
+  let li = $(
+    '<a href=#><li class="book">'+
+      '<div><h1><b>'+book.title +'</b></h1></div>'+
+      '<div><img src="'+book.image+'" width="80%"/></div>'+
+      '<div><h3>'+ book.author +'</h3></div>'+
+      '<div>'+ book.releaseDate+'</div>'+
+      '<div></a>'+
+        '<button class="editButton saveButton">Edit</button>'+
+        '<button class="deleteButton">Delete</button>'+
+      '</div>'+
+    '</li>'
+  )
+  li.data('book', book)
+  $(".list-group").append(li)
+}
+
+function getAllBooks() {
+  $.ajax({url: "https://mutably.herokuapp.com/books", 
+    success: function(result) {
+      let books = result.books
+      console.log(books.length)
+      for (let i = 0; i < books.length; i++) {
+        addBook(books[i])
+      } 
+    }
+  })
+} 
+
+$(document).ready(function(){
+  getAllBooks()
+})
+
+$(document).on ('click', "#newBook", function(form) {
+  event.preventDefault()
+  info = {
+    title: $('#title').val(),
+    image: $('#image').val(),
+    author: $('#author').val(),
+    releaseDate: $('#releaseDate').val(), 
+  }
+  $.ajax({
+    url: "https://mutably.herokuapp.com/books",
+    method: "POST",
+    data: info
+  }) 
+  addBook(info)
+})
+
+$(document).on('click', '.deleteButton', function(event) {
+  let li = $(event.target).closest('.book')
+  let id = li.data('book')._id
+  $.ajax({ 
+    method: 'DELETE',
+    url: 'https://mutably.herokuapp.com/books/'+id,
+    success: function() {
+      console.log('success')
+      li.fadeOut( function(){
+        li.remove()
+      })
+    }
+  }) 
+})
+
+
+$(document).on('click', '.editButton', function(value){
+
+
+  // let form = $(
+  //   '<a href=#><li class="book">'+
+  //     '<div><h1><b>'+book.title +'</b></h1></div>'+
+  //     '<div><img src="'+book.image+'" width="80%"/></div>'+
+  //     '<div><h3>'+ book.author +'</h3></div>'+
+  //     '<div>'+ book.releaseDate+'</div>'+
+  //     '<div></a>'+
+  //       '<button class="editButton">Edit</button>'+
+  //       '<button class="deleteButton">Delete</button>'+
+  //     '</div>'+
+  //   '</li>'
+  // )
+  // form.data('form', form)
+
+  if ($('.editButton').html() == "Edit") {
+    $('.editButton').html("Save")
+  } else {
+    $('.editButton').html("Edit")
+  }
+})
+
+
+// $(document).on('click', '.editButton', function(value){
+//     $(".editButton").html('Edit', 'Save') 
+// })
+
+
+  // else {
+  //   $(".editButton").html('Edit')
+  // }
+//   let li = $(event.target).closest('.book')
+//   let id = li.data('book')._id
+//   $.ajax({ 
+//     method: 'DELETE',
+//     url: 'https://mutably.herokuapp.com/books/'+id,
+//     success: function() {
+//       console.log('success')
+//       li.fadeIn( function(){
+//         console.log('hello')
+//       })
+//     }
+//   })
+// })
+
+console.log("You've reached the end!")
--- server.js
+++ server.js
@@ -1,43 +1,14 @@
 const express = require('express')
 const app = express()
-const request = require('request')
 app.use(express.static('public'))
 
 // set 'html' as the engine, using ejs's renderFile function
-
 var ejs = require('ejs');
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
-let books;
-
-request('https://mutably.herokuapp.com/books', (err, res, body) => {
-  books = body
-  console.log(body)
-})
-
 app.get('/', (req, res) => {
-  res.render('index', {books: books})
-})
-
-app.get('/books', (req, res) => {
-  res.redirect('/')
-})
-
-app.post('/books', (req, res) => {
-  res.redirect('/')
-})
-
-app.get('/books/:id', (req, res) => {
-  res.redirect('/')
-})
-
-app.put('/books:id', (req, res) => {
-  res.redirect('/')
-})
-
-app.delete('/books/:id', (req, res) => {
-  res.redirect('/')
+  res.render('index')
 })
 
 const port = process.env.PORT || 3000
